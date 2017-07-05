7d4053290c762f33804ebb8d777207d70ea179f7
Constantina <polyccon@gmail.com>
2017-07-05 16:50:17 +0100

    third test for delete function

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,10 +1,13 @@
 var test = require('tape');
 var logic = require('./logic.js');
 
+var todoarray = [{id: 1, txt: 'go for run'}, {id: 4, txt: 'drink water'}];
+
 // test('Example test', function(t) {
 //   t.pass();
 //   t.end();
 // });
+
 
 // tests for deleteTodo
 test('deleteTodo should return an array', function(t) {
@@ -19,5 +22,14 @@ test('new array should not contain todo with id of idToDelete', function(t) {
   var actual = logic.deleteTodo([{id: 1, txt: 'go for run'}, {id: 2, txt: 'drink water'}], 2);
   var expected = [{id: 1, txt: 'go for run'}];
   t.deepEqual(actual, expected, 'should delete object with id idToDelete');
+  t.end();
+});
+
+test('should leave the input argument todos unchanged', function(t) {
+
+  logic.deleteTodo(todoarray, 4);
+  var actual = todoarray;
+  var expected = [{id: 1, txt: 'go for run'}, {id: 4, txt: 'drink water'}];
+  t.deepEqual(actual, expected, 'should leave the input argument todos unchanged');
   t.end();
 });
